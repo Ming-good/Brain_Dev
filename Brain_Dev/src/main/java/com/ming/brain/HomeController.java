210d279cdb64c82a5e576cdf2090bbdba138583f
read1 <read1@LAPTOP-I3EEAPQP>
2020-10-02 20:18:19 +0900

    롬복, 오라클 메이븐에 추가

--- Brain_Dev/src/main/java/com/ming/brain/HomeController.java
+++ Brain_Dev/src/main/java/com/ming/brain/HomeController.java
@@ -1,23 +1,25 @@
 package com.ming.brain;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
+import java.util.HashMap;
 
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Handles requests for the application home page.
  */
 @Controller
 public class HomeController {
-	
+ 
+	@Autowired
+    private SqlSessionFactory sqlFactory;	
+    
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
 	/**
@@ -25,7 +27,9 @@ public class HomeController {
 	 */
 	@GetMapping("/events")
 	public String home(Model model) {
-		
+        SqlSession session = sqlFactory.openSession();
+        HashMap map= session.selectOne("com.ming.brain.mapperInterface.selectTest");
+        System.out.println(map.get("NAME"));		
 		return "home";
 	}
 	
