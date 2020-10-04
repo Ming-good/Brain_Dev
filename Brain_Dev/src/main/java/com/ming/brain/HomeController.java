dfaddef8b88f7d05242b64ad691ada0974979323
read1 <read1@LAPTOP-I3EEAPQP>
2020-10-04 17:49:17 +0900

    Chapter02 DispacherServlet에 rootContext와 servletContext가 만들어진 방법

--- Brain_Dev/src/main/java/com/ming/brain/HomeController.java
+++ Brain_Dev/src/main/java/com/ming/brain/HomeController.java
@@ -2,20 +2,24 @@ package com.ming.brain;
 
 import java.util.HashMap;
 
+import javax.servlet.http.HttpServlet;
+
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.context.WebApplicationContext;
 
 /**
  * Handles requests for the application home page.
  */
 @Controller
-public class HomeController {
+public class HomeController extends HttpServlet {
  
 	@Autowired
     private SqlSessionFactory sqlFactory;	
@@ -28,8 +32,12 @@ public class HomeController {
 	@GetMapping("/events")
 	public String home(Model model) {
         SqlSession session = sqlFactory.openSession();
-        HashMap map= session.selectOne("com.ming.brain.mapperInterface.selectTest");
+        HashMap map= session.selectOne("com.ming.brain.mapperInterface.TestMapper.selectTest");
         System.out.println(map.get("NAME"));		
+        
+        ApplicationContext context = (ApplicationContext) getServletContext().getAttribute(WebApplicationContext.ROOT_WEB_APPLICATION_CONTEXT_ATTRIBUTE);
+        HellowService service = context.getBean(HellowService.class);
+        String name = service.getName();
 		return "home";
 	}
 	
