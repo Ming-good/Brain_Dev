3968d6298dc8f32363d03ff44ce5ff9f1de6613c
read1 <read1@LAPTOP-I3EEAPQP>
2020-11-30 00:23:06 +0900

    SQL 로그 찍기

--- Brain_Dev/src/main/java/com/ming/brain/ctr/HomeController.java
+++ Brain_Dev/src/main/java/com/ming/brain/ctr/HomeController.java
@@ -2,9 +2,12 @@ package com.ming.brain.ctr;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.ming.brain.mapperInterface.TestMapper;
 
 /**
  * Handles requests for the application home page.
@@ -14,9 +17,13 @@ public class HomeController {
  
 	private final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
+	@Autowired
+	TestMapper test;
+	
 	@GetMapping("/events")
-	public String home(Model model) {
+	public String home(Model model) throws Exception {
 		model.addAttribute("name", "이민규");
+		System.out.println(test.selectTest());
 		logger.debug("로그테스트");
 		return "/main/home";
 	}
